5133915383d14e6561c1d089d0ac74602bb15685
auranbuckles <auran59@gmail.com>
2017-11-01 13:40:56 -0700

    update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,35 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('59fa1f37b65c22a571a4b197')
+  // }, {
+  //   // MongoDB update operators
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('59f94e65cc56e80d58ffdc35')
+  }, {
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
